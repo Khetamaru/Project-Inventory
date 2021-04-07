c1244ae04a836501b9b1910504a79f384916fec2
Khetamaru <lasserre@intechinfo.fr>
2021-04-07 16:14:24 +0200

    07/03/21
    First try code injection + introduction to XAML
    Some Useless code to delete later

---
+++ Project Inventory/Project Inventory/Utilisateur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Inventory
+{
+    class Utilisateur
+    {
+        public string Nom { get; set; }
+        public HorizontalAlignment horizontalAlign { get; set; }
+        public VerticalAlignment verticalAlign { get; set; }
+    }
+}
